f136afce0b3d7640147b4b709f3227313d1e4388
Askar <ivexgod@gmail.com>
2021-08-20 13:28:25 +0500

    Init

---
+++ src/main/java/ru/dromran/testtz/TesttzApplication.java
@@ -0,0 +1,13 @@
+package ru.dromran.testtz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesttzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dromran/testtz/TesttzApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dromran.testtz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesttzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
